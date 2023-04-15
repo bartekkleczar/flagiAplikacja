ec3b729e66905e5ea444bc70e59c1c0f4cf181d3
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 15:06:46 +0200

    dodanie kodu dla przycisku przechodzacego do MA

--- app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
@@ -1,11 +1,20 @@
 package pl.klenczi.flagieuropy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val filter = findViewById<Button>(R.id.btnFilter)
+
+        filter.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
