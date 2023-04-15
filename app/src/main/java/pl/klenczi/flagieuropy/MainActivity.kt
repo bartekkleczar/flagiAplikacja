5acfac77fe0bca13f35f3fe21dde90160e7697c2
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 23:48:22 +0200

    dodanie krajow na b

--- app/src/main/java/pl/klenczi/flagieuropy/MainActivity.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/MainActivity.kt
@@ -27,11 +27,16 @@ class MainActivity : AppCompatActivity() {
         val austria = Panstwo("Austria", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.austria)
         val azerbejdzan = Panstwo("Azerbejdzan", u = "poziome", z = true, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.azerbejdzan)
         val anglia = Panstwo("Anglia", u = "krzyz", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.anglia)
+        val bialorus = Panstwo("Bialorus", u = "poziome", z = true, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.bialorus)
+        val bosnia = Panstwo("Bosnia", u = "inne", z = true, cze = 0, bi = 1, ni = 1, cza = 0, image = R.drawable.bosnia)
+        val bulgaria = Panstwo("Bulgaria", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.bulgaria)
+        val chorwacja = Panstwo("Chorwacja", u = "poziome", z = true, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.chorwacja)
         val niemcy = Panstwo("Niemcy", u = "poziome", z = false, cze = 1, bi = 0, ni = 0, cza = 1, image = R.drawable.niemcy)
         val polska = Panstwo("Polska", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.polska)
 
         val panstwa = mutableListOf<Panstwo>(
-            albania, andora, armenia, austria, azerbejdzan, anglia, niemcy, polska
+            albania, andora, armenia, austria, azerbejdzan, anglia, bialorus,
+            bosnia, bulgaria, chorwacja, niemcy, polska
         )
 
         val panstwaPoKolorach = mutableListOf<Panstwo>()
@@ -44,15 +49,14 @@ class MainActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this)
 
         val intent = intent
-        val czerwony = intent.getIntExtra("czerwony", 2)
-        val bialy = intent.getIntExtra("bialy", 2)
-        val niebieski = intent.getIntExtra("niebieski", 2)
-        val czarny = intent.getIntExtra("czarny", 2)
+        val czerwony = intent.getIntExtra("czerwony", 0)
+        val bialy = intent.getIntExtra("bialy", 0)
+        val niebieski = intent.getIntExtra("niebieski", 0)
+        val czarny = intent.getIntExtra("czarny", 0)
         val pozycja = intent.getStringExtra("pozycja")
 
         filterKolory(panstwa, panstwaPoKolorach, cze = czerwony, bi = bialy, ni = niebieski, cza = czarny)
-        if (pozycja != null){ filterUlozenie(panstwaPoKolorach, panstwaPoUlozeniu, u = pozycja) }
-        else { filterUlozenie(panstwaPoKolorach, panstwaPoUlozeniu, u = "dow") }
+        if(pozycja != "dow"){if(pozycja != null){ filterUlozenie(panstwaPoKolorach, panstwaPoUlozeniu, u = pozycja) }}
         //filterZnaczek(panstwaPoUlozeniu, panstwaPoZnaczku, z = true)
 
         fun ostatiaLista(p: MutableList<Panstwo>, pk: MutableList<Panstwo>, pu: MutableList<Panstwo>, pz: MutableList<Panstwo>): MutableList<Panstwo> {
