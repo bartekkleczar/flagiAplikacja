5e7f231af687ff3c9e2a2c598dd4fa2288944524
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 21:15:18 +0200

    dodanie kodu dla checkBoxa czarnego

--- app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,22 @@ class StartActivity : AppCompatActivity() {
 
         val filter = findViewById<Button>(R.id.btnFilter)
 
+        val cbCzarny = findViewById<CheckBox>(R.id.cbCzarny)
+        var czarny = 0
+        cbCzarny.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                czarny = 1
+
+            } else {
+                czarny = 0
+            }
+        }
+
         filter.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("czarny", czarny)
+
             startActivity(intent)
         }
     }
