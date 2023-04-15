47bef32851243387992bd83c4cdb218726f91cb2
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 21:19:08 +0200

    dodanie kodu do switcha i dodanie jego odslugi w checkBoxie dla czaenwgo

--- app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/StartActivity.kt
@@ -5,22 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.Switch
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
+        var switch = 0
+        var czarny = 0
+
+        val sw = findViewById<Switch>(R.id.swIle)
+        sw.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                switch = 1
+            } else {
+                switch = 0
+            }
+        }
+
         val filter = findViewById<Button>(R.id.btnFilter)
 
         val cbCzarny = findViewById<CheckBox>(R.id.cbCzarny)
-        var czarny = 0
+
         cbCzarny.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 czarny = 1
 
             } else {
-                czarny = 0
+                if(switch == 0){czarny = 2}
             }
         }
 
