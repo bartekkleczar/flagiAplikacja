e9e7895631b7ed24a652c1df083e555da595e3e4
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 23:18:30 +0200

    zmiana w funkcji filterKolory

--- app/src/main/java/pl/klenczi/flagieuropy/Funkcje.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/Funkcje.kt
@@ -2,10 +2,10 @@ package pl.klenczi.flagieuropy
 
 fun filterKolory(listaInput: MutableList<Panstwo>, listaOutput: MutableList<Panstwo>, cze: Int = 2, bi : Int = 2, ni : Int = 2, cza : Int = 2 ){
     for(i in listaInput){
-        if((cze== i.cze|| cze== 2)){
-            if((bi == i.bi || bi == 2)){
-                if((ni== i.ni|| ni== 2)){
-                    if((cza == i.cza || cza == 2)){
+        if((cze== i.cze|| i.cze== 2)){
+            if((bi == i.bi || i.bi == 2)){
+                if((ni== i.ni|| i.ni== 2)){
+                    if((cza == i.cza || i.cza == 2)){
                         listaOutput.add(i)
                     }
                 }
@@ -14,7 +14,7 @@ fun filterKolory(listaInput: MutableList<Panstwo>, listaOutput: MutableList<Pans
     }
 }
 
-fun filterUlozenie(listaInput: MutableList<Panstwo>, listaOutput: MutableList<Panstwo>, u: String = "inne"){
+fun filterUlozenie(listaInput: MutableList<Panstwo>, listaOutput: MutableList<Panstwo>, u: String = "dow"){
     for(i in listaInput){
         if(u == i.u){
             listaOutput.add(i)
