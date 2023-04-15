c9019954db369694b46daf482b694a7eccff5421
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 16:20:48 +0200

    dodanie do data klasy zmiennej image: Int do id zdjecia

--- app/src/main/java/pl/klenczi/flagieuropy/klasaFlagi.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/klasaFlagi.kt
@@ -1,10 +1,12 @@
 package pl.klenczi.flagieuropy
 
-data class Panstwo(var n: String, var u: String = "inne",
-                      var z: Boolean = false,
-                      var cze: Boolean = true, var bi: Boolean = true,
-                      var ni: Boolean = true, var cza: Boolean = true,
-                      var image: String)
+data class Panstwo(
+    var n: String, var u: String = "inne",
+    var z: Boolean = false,
+    var cze: Boolean = true, var bi: Boolean = true,
+    var ni: Boolean = true, var cza: Boolean = true,
+    var image: Int
+)
 
 
 
