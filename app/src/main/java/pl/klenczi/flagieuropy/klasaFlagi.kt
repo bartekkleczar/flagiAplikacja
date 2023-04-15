c02e74ac4fc45287d3e47542ecf2485454a5a40a
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-15 15:32:05 +0200

    dodanie do klasy Panstwo stringa image na scierzke do zdjecia

--- app/src/main/java/pl/klenczi/flagieuropy/klasaFlagi.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/klasaFlagi.kt
@@ -3,7 +3,8 @@ package pl.klenczi.flagieuropy
 data class Panstwo(var n: String, var u: String = "inne",
                       var z: Boolean = false,
                       var cze: Boolean = true, var bi: Boolean = true,
-                      var ni: Boolean = true, var cza: Boolean = true)
+                      var ni: Boolean = true, var cza: Boolean = true,
+                      var image: String)
 
 
 
