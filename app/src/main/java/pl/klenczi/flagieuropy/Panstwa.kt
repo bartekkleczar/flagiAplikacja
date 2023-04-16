a356d3f76a08b7c83ccf891d2cb8a3129561a8b4
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-16 20:59:55 +0200

    dodanie obiektow reszty panstw europy

--- app/src/main/java/pl/klenczi/flagieuropy/Panstwa.kt
+++ app/src/main/java/pl/klenczi/flagieuropy/Panstwa.kt
@@ -18,16 +18,43 @@ val finlandia = Panstwo("Finlandia", u = "krzyz", z = false, cze = 0, bi = 1, ni
 val francja = Panstwo("Francja", u = "pionowe", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.francja)
 val grecja = Panstwo("Grecja", u = "poziome", z = false, cze = 0, bi = 1, ni = 1, cza = 0, image = R.drawable.grecja)
 val hiszpania = Panstwo("Hiszpania", u = "poziome", z = true, cze = 1, bi = 0, ni = 0, cza = 0, image = R.drawable.hiszpania)
-val holandia = Panstwo("Holandia", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.holandia)
+val holandia = Panstwo("Holandia", u = "poziome", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.holandia)
 val irlandia = Panstwo("Irlandia", u = "pionowe", z = false, cze = 0, bi = 1, ni = 0, cza = 0, image = R.drawable.irlandia)
-
+val islandia = Panstwo("Islandia", u = "krzyz", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.islandia)
+val kazachstan = Panstwo("Kazachstan", u = "inne", z = true, cze = 0, bi = 0, ni = 1, cza = 0, image = R.drawable.kazachstan)
+val liechtenstein = Panstwo("Liechtenstein", u = "poziome", z = true, cze = 1, bi = 0, ni = 1, cza = 0, image = R.drawable.liechtenstein)
+val litwa = Panstwo("Litwa", u = "poziome", z = false, cze = 1, bi = 0, ni = 0, cza = 0, image = R.drawable.litwa)
+val luksemburg = Panstwo("Luksemburg", u = "poziome", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.luksemburg)
+val lotwa = Panstwo("Łotwa", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.lotwa)
+val macedonia = Panstwo("Macedonia", u = "inne", z = true, cze = 1, bi = 0, ni = 0, cza = 0, image = R.drawable.macedonia)
+val malta = Panstwo("Malta", u = "pionowe", z = true, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.malta)
+val moldawia = Panstwo("Moldawia", u = "pionowe", z = true, cze = 1, bi = 0, ni = 1, cza = 0, image = R.drawable.moldawia)
+val monako = Panstwo("Monako", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.monako)
 val niemcy = Panstwo("Niemcy", u = "poziome", z = false, cze = 1, bi = 0, ni = 0, cza = 1, image = R.drawable.niemcy)
+val norwegia = Panstwo("Norwegia", u = "krzyz", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.norwegia)
 val polska = Panstwo("Polska", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.polska)
+val portugalia = Panstwo("Portugalia", u = "pionowe", z = true, cze = 1, bi = 0, ni = 0, cza = 0, image = R.drawable.portugalia)
+val rosja = Panstwo("Rosja", u = "poziome", z = false, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.rosja)
+val rumunia = Panstwo("Rumunia", u = "pionowe", z = false, cze = 1, bi = 0, ni = 1, cza = 0, image = R.drawable.rumunia)
+val sanmarino = Panstwo("San Marino", u = "poziome", z = true, cze = 0, bi = 1, ni = 1, cza = 0, image = R.drawable.sanmarino)
+val serbia = Panstwo("Seriba", u = "poziome", z = true, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.serbia)
+val slowacja = Panstwo("Slowacja", u = "poziome", z = true, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.slowacja)
+val slowenia = Panstwo("Slowenia", u = "poziome", z = true, cze = 1, bi = 1, ni = 1, cza = 0, image = R.drawable.slowenia)
+val szwajcaria = Panstwo("Szwajcaria", u = "krzyz", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.szwajcaria)
+val szwecja = Panstwo("Szwecja", u = "krzyz", z = false, cze = 0, bi = 0, ni = 1, cza = 0, image = R.drawable.szwecja)
+val turcja = Panstwo("Turcja", u = "inne", z = true, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.turcja)
+val ukraina = Panstwo("Ukraina", u = "poziome", z = false, cze = 0, bi = 0, ni = 1, cza = 0, image = R.drawable.ukraina)
+val watykan = Panstwo("Watykan", u = "pionowe", z = true, cze = 0, bi = 1, ni = 0, cza = 0, image = R.drawable.watykan)
+val wegry = Panstwo("Wegry", u = "poziome", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.wegry)
+val wlochy = Panstwo("Wlochy", u = "pionowe", z = false, cze = 1, bi = 1, ni = 0, cza = 0, image = R.drawable.wlochy)
+
 
 val panstwa = mutableListOf<Panstwo>(
     albania, andora, armenia, austria, azerbejdzan, anglia, bialorus,
     bosnia, bulgaria, chorwacja, czarnogora, czechy, dania, estonia,
-    finlandia, francja, grecja, hiszpania, holandia, irlandia,
-
-    niemcy, polska
+    finlandia, francja, grecja, hiszpania, holandia, irlandia, islandia,
+    kazachstan, liechtenstein, litwa, luksemburg, lotwa, macedonia, malta,
+    moldawia, monako,niemcy, norwegia, polska, portugalia, rosja, rumunia,
+    sanmarino, serbia, slowacja, slowenia, szwajcaria, szwecja, turcja,
+    ukraina, watykan, wegry, wlochy
 )
